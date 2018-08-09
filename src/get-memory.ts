e7665354d60cfc9ebb758fa6075fd296508bf9ba
Huan LI <zixia@zixia.net>
2018-08-10 03:12:57 +0800

    better AWS env var name

--- src/get-memory.ts
+++ src/get-memory.ts
@@ -15,25 +15,25 @@ export function getMemory (name: string): MemoryCard {
     return memory
   }
 
-  const S3_ACCESS_KEY_ID     = process.env.S3_ACCESS_KEY_ID as string
-  const S3_SECRET_ACCESS_KEY = process.env.S3_SECRET_ACCESS_KEY as string
-  const S3_REGION            = process.env.S3_REGION as string
-  const S3_BUCKET            = process.env.S3_BUCKET as string
+  const AWS_ACCESS_KEY_ID     = process.env.AWS_ACCESS_KEY_ID as string
+  const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY as string
+  const AWS_REGION            = process.env.AWS_REGION as string
+  const AWS_S3_BUCKET         = process.env.AWS_S3_BUCKET as string
 
-  if (  S3_ACCESS_KEY_ID
-    &&  S3_BUCKET
-    &&  S3_REGION
-    &&  S3_SECRET_ACCESS_KEY
+  if (  AWS_ACCESS_KEY_ID
+    &&  AWS_REGION
+    &&  AWS_SECRET_ACCESS_KEY
+    &&  AWS_S3_BUCKET
   ) {
     log.verbose('getMemory', 'getMemory() creating new s3 memory')
 
     memory = new MemoryCard({
       name,
       storageOptions: {
-        accessKeyId     : S3_ACCESS_KEY_ID,
-        bucket          : S3_BUCKET,
-        region          : S3_REGION,
-        secretAccessKey : S3_SECRET_ACCESS_KEY,
+        accessKeyId     : AWS_ACCESS_KEY_ID,
+        bucket          : AWS_S3_BUCKET,
+        region          : AWS_REGION,
+        secretAccessKey : AWS_SECRET_ACCESS_KEY,
         type            : 's3',
       }
     })
