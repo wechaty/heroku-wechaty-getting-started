86d11a3284c95106bae8f4f25feffdce9eaf139c
Huan LI <zixia@zixia.net>
2019-12-19 03:17:38 +0800

    better login version msg

--- src/handlers/on-login.ts
+++ src/handlers/on-login.ts
@@ -1,15 +1,19 @@
 import {
   Contact,
   log,
-  VERSION,
+  VERSION as WECHATY_VERSION,
   Wechaty,
-}             from 'wechaty'
+}                               from 'wechaty'
+
+import {
+  VERSION as BOT_VERSION,
+}                               from '../config'
 
 export default async function onLogin (
   this : Wechaty,
   user : Contact,
 ): Promise<void> {
-  const msg = `${user.name()} Heroku Wechaty Getting Started v${VERSION} logined`
+  const msg = `${user.name()} Wechaty(v${WECHATY_VERSION}) Heroku Bot(v${BOT_VERSION}) Getting Started logined`
   log.info('startBot', 'onLogin(%s) %s', user, msg)
   await user.say(msg)
 }
